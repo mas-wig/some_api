618e83d55e01c3e5348169df10f27e5c3c2ff355
wigtono <mas_wig@protonmail.ch>
2023-11-02 13:58:34 +0700

    Implement RefreshToken and LogOutUser Method...

--- api/auth_handler.go
+++ api/auth_handler.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"fmt"
 	"net/http"
 	"strings"
 
@@ -24,21 +25,21 @@ func NewAuthHandle(authService services.AuthService, userService services.UserSe
 func (a *AuthHandler) SignUpUser(c *gin.Context) {
 	var credentials *types.RegisterInput
 	if err := c.ShouldBindJSON(&credentials); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Bad Request", "message": err.Error()})
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "Bad Request", "message": err.Error()})
 		return
 	}
 	if credentials.Password != credentials.PasswordConfirm {
-		c.JSON(http.StatusBadRequest, gin.H{"message": "Bad Request", "data": "password not match"})
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"message": "Bad Request", "data": "password not match"})
 		return
 	}
 
 	newUser, err := a.authService.RegisterUser(credentials)
 	if err != nil {
 		if strings.Contains(err.Error(), "email already exist") {
-			c.JSON(http.StatusBadRequest, gin.H{"message": "Bad Request", "data": err.Error()})
+			c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"message": "Bad Request", "data": err.Error()})
 			return
 		}
-		c.JSON(http.StatusBadGateway, gin.H{"status": "error", "message": err.Error()})
+		c.AbortWithStatusJSON(http.StatusBadGateway, gin.H{"status": "error", "message": err.Error()})
 		return
 	}
 	c.JSON(http.StatusCreated, gin.H{"message": "success", "data": newUser})
@@ -47,21 +48,21 @@ func (a *AuthHandler) SignUpUser(c *gin.Context) {
 func (a *AuthHandler) SignInUser(c *gin.Context) {
 	var credentials *types.LoginInput
 	if err := c.ShouldBindJSON(&credentials); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Bad Request", "message": err.Error()})
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "Bad Request", "message": err.Error()})
 		return
 	}
 
 	loginUser, err := a.userService.FindUserByEmail(credentials.Email)
 	if err != nil {
 		if err == mongo.ErrNoDocuments {
-			c.JSON(http.StatusBadRequest, gin.H{"message": "Bad Request", "data": err.Error()})
+			c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"message": "Bad Request", "data": err.Error()})
 			return
 		}
 		return
 	}
 
 	if err := utils.VerifyPassword(loginUser.Password, credentials.Password); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"message": "Bad Request", "data": err.Error()})
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"status": "fail", "message": err.Error()})
 		return
 	}
 
@@ -69,12 +70,12 @@ func (a *AuthHandler) SignInUser(c *gin.Context) {
 
 	accessToken, err := utils.CreateToken(config.AccessTokenExpiresIn, loginUser.ID, config.AccessTokenPrivateKey)
 	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"message": "Bad Request", "data": err.Error()})
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"status": "fail", "message": err.Error()})
 		return
 	}
 	refreshToken, err := utils.CreateToken(config.RefreshTokenExpiresIn, loginUser.ID, config.RefreshTokenPrivateKey)
 	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"status": "fail", "message": err.Error()})
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"status": "fail", "message": err.Error()})
 		return
 	}
 
@@ -82,4 +83,41 @@ func (a *AuthHandler) SignInUser(c *gin.Context) {
 	c.SetCookie("refresh_token", refreshToken, config.RefreshTokenMaxAge*60, "/", "localhost", false, true)
 	c.SetCookie("logged_in", "true", config.AccessTokenMaxAge*60, "/", "localhost", false, false)
 	c.JSON(http.StatusOK, gin.H{"status": "success", "access_token": accessToken})
+}
+
+func (a *AuthHandler) RefreshAccessToken(c *gin.Context) {
+	cookie, err := c.Cookie("refresh_token")
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusForbidden, gin.H{"status": "Forbidden", "message": "could not refresh access token!!"})
+		return
+	}
+
+	config, _ := config.LoadConfig("..")
+	sub, err := utils.ValidateToken(cookie, config.RefreshTokenPublicKey)
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusForbidden, gin.H{"status": "Forbidden", "message": err.Error()})
+		return
+	}
+
+	user, err := a.userService.FindUserByID(fmt.Sprint(sub))
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusForbidden, gin.H{"status": "Forbidden", "message": err.Error()})
+		return
+	}
+
+	accessToken, err := utils.CreateToken(config.AccessTokenExpiresIn, user.ID, config.AccessTokenPrivateKey)
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusForbidden, gin.H{"status": "Forbidden", "message": err.Error()})
+	}
+
+	c.SetCookie("access_token", accessToken, config.AccessTokenMaxAge*60, "/", "localhost", false, true)
+	c.SetCookie("logged_in", "true", config.AccessTokenMaxAge*60, "/", "localhost", false, false)
+	c.JSON(http.StatusOK, gin.H{"status": "success", "access_token": accessToken})
+}
+
+func (a *AuthHandler) LogoutUser(c *gin.Context) {
+	c.SetCookie("access_token", "", -1, "/", "localhost", false, true)
+	c.SetCookie("refresh_token", "", -1, "/", "localhost", false, true)
+	c.SetCookie("logged_in", "", -1, "/", "localhost", false, true)
+	c.JSON(http.StatusOK, gin.H{"status": "success"})
 }
