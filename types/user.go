737bfe4bb2e5371ef5af93d5b2f806cc5c53dd95
wigtono <mas_wig@protonmail.ch>
2023-11-05 23:13:22 +0700

    Just do some gopls code actions waring recomendation eg. rerange structure user types

--- types/user.go
+++ types/user.go
@@ -7,14 +7,17 @@ import (
 )
 
 type RegisterInput struct {
-	CreatedAt       time.Time `json:"created_at" bson:"created_at"`
-	UpdatedAt       time.Time `json:"updated_at" bson:"updated_at"`
-	Name            string    `json:"name" bson:"name" binding:"required"`
-	Email           string    `json:"email" bson:"email" binding:"required"`
-	Password        string    `json:"password" bson:"password" binding:"required,min=8"`
-	PasswordConfirm string    `json:"passwordConfirm" bson:"passwordConfirm,omitempty" binding:"required"`
-	Role            string    `json:"role" bson:"role"`
-	Verified        bool      `json:"verified" bson:"verified"`
+	ResetPasswordAt    time.Time `json:"resetPasswordAt,omitempty" bson:"resetPasswordAt,omitempty"`
+	CreatedAt          time.Time `json:"created_at" bson:"created_at"`
+	UpdatedAt          time.Time `json:"updated_at" bson:"updated_at"`
+	Name               string    `json:"name" bson:"name" binding:"required"`
+	Email              string    `json:"email" bson:"email" binding:"required"`
+	Password           string    `json:"password" bson:"password" binding:"required,min=8"`
+	PasswordConfirm    string    `json:"passwordConfirm" bson:"passwordConfirm,omitempty" binding:"required"`
+	Role               string    `json:"role" bson:"role"`
+	VerificationCode   string    `json:"verificationCode,omitempty" bson:"verificationCode,omitempty"`
+	ResetPasswordToken string    `json:"resetPasswordToken,omitempty" bson:"resetPasswordToken,omitempty"`
+	Verified           bool      `json:"verified" bson:"verified"`
 }
 
 type LoginInput struct {
@@ -23,35 +26,58 @@ type LoginInput struct {
 }
 
 type DBResponse struct {
-	CreatedAt       time.Time          `json:"created_at" bson:"created_at"`
-	UpdatedAt       time.Time          `json:"updated_at" bson:"updated_at"`
-	Name            string             `json:"name" bson:"name"`
-	Email           string             `json:"email" bson:"email"`
-	Password        string             `json:"password" bson:"password"`
-	PasswordConfirm string             `json:"passwordConfirm" bson:"passwordConfirm"`
-	Role            string             `json:"role" bson:"role"`
-	ID              primitive.ObjectID `json:"id" bson:"_id"`
-	Verified        bool               `json:"verified" bson:"verified"`
+	ResetPasswordAt    time.Time          `json:"resetPasswordAt,omitempty" bson:"resetPasswordAt,omitempty"`
+	CreatedAt          time.Time          `json:"created_at" bson:"created_at"`
+	UpdatedAt          time.Time          `json:"updated_at" bson:"updated_at"`
+	Name               string             `json:"name" bson:"name"`
+	Email              string             `json:"email" bson:"email"`
+	Password           string             `json:"password" bson:"password"`
+	PasswordConfirm    string             `json:"passwordConfirm,omitempty" bson:"passwordConfirm,omitempty"`
+	Role               string             `json:"role" bson:"role"`
+	VerificationCode   string             `json:"verificationCode,omitempty" bson:"verificationCode"`
+	ResetPasswordToken string             `json:"resetPasswordToken,omitempty" bson:"resetPasswordToken,omitempty"`
+	ID                 primitive.ObjectID `json:"id" bson:"_id"`
+	Verified           bool               `json:"verified" bson:"verified"`
+}
+
+type UpdateInput struct {
+	ResetPasswordAt    time.Time `json:"resetPasswordAt,omitempty" bson:"resetPasswordAt,omitempty"`
+	CreatedAt          time.Time `json:"created_at,omitempty" bson:"created_at,omitempty"`
+	UpdatedAt          time.Time `json:"updated_at,omitempty" bson:"updated_at,omitempty"`
+	Name               string    `json:"name,omitempty" bson:"name,omitempty"`
+	Email              string    `json:"email,omitempty" bson:"email,omitempty"`
+	Password           string    `json:"password,omitempty" bson:"password,omitempty"`
+	Role               string    `json:"role,omitempty" bson:"role,omitempty"`
+	VerificationCode   string    `json:"verificationCode,omitempty" bson:"verificationCode,omitempty"`
+	ResetPasswordToken string    `json:"resetPasswordToken,omitempty" bson:"resetPasswordToken,omitempty"`
+	Verified           bool      `json:"verified,omitempty" bson:"verified,omitempty"`
 }
 
 type UserResponse struct {
 	CreatedAt time.Time          `json:"created_at" bson:"created_at"`
 	UpdatedAt time.Time          `json:"updated_at" bson:"updated_at"`
-	Name      string             `json:"name,omitempty" bson:"name"`
-	Email     string             `json:"email,omitempty" bson:"email"`
-	Role      string             `json:"role,omitempty" bson:"role"`
-	ID        primitive.ObjectID `json:"id,omitempty" bson:"_id"`
-	Verified  bool               `json:"verified" bson:"verified"`
+	Name      string             `json:"name,omitempty" bson:"name,omitempty"`
+	Email     string             `json:"email,omitempty" bson:"email,omitempty"`
+	Role      string             `json:"role,omitempty" bson:"role,omitempty"`
+	ID        primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
 }
 
-// Jadi gak semata mata langsung ontput semua data dari database
+type ForgotPasswordInput struct {
+	Email string `json:"email" bson:"email" binding:"required"`
+}
+
+type ResetPasswordInput struct {
+	Password        string `json:"password" bson:"password"`
+	PasswordConfirm string `json:"passwordConfirm,omitempty" bson:"passwordConfirm,omitempty"`
+}
+
 func FilteredResponse(user *DBResponse) UserResponse {
 	return UserResponse{
+		ID:        user.ID,
+		Email:     user.Email,
+		Name:      user.Name,
+		Role:      user.Role,
 		CreatedAt: user.CreatedAt,
 		UpdatedAt: user.UpdatedAt,
-		Name:      user.Name,
-		Email:     user.Email,
-		Role:      user.Role,
-		ID:        user.ID,
 	}
 }
