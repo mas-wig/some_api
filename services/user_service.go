f43e2567605e14362c8d816c7c8cb952759bc5a6
wigtono <mas_wig@protonmail.ch>
2023-11-05 23:12:00 +0700

    Implement service for update user by id use by email verification chech `api/auth_handler.go`

--- services/user_service.go
+++ services/user_service.go
@@ -5,4 +5,5 @@ import "github.com/mas-wig/post-api-1/types"
 type UserService interface {
 	FindUserByID(id string) (*types.DBResponse, error)
 	FindUserByEmail(email string) (*types.DBResponse, error)
+	UpdateUserByID(id string, data *types.UpdateInput) (*types.DBResponse, error)
 }
--- services/user_service_impl.go
+++ services/user_service_impl.go
@@ -2,12 +2,15 @@ package services
 
 import (
 	"context"
+	"errors"
 	"strings"
 
 	"github.com/mas-wig/post-api-1/types"
+	"github.com/mas-wig/post-api-1/utils"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type UserServiceImpl struct {
@@ -49,4 +52,23 @@ func (us *UserServiceImpl) FindUserByEmail(email string) (*types.DBResponse, err
 		return nil, err
 	}
 	return user, nil
+}
+
+func (us *UserServiceImpl) UpdateUserByID(id string, data *types.UpdateInput) (*types.DBResponse, error) {
+	doc, err := utils.ToDocument(data)
+	if err != nil {
+		return &types.DBResponse{}, err
+	}
+
+	objID, _ := primitive.ObjectIDFromHex(id)
+	var (
+		updatedUser *types.DBResponse
+		query       = bson.D{{Key: "_id", Value: objID}}
+		update      = bson.D{{Key: "$set", Value: doc}}
+		result      = us.collection.FindOneAndUpdate(us.ctx, query, update, options.FindOneAndUpdate().SetReturnDocument(1))
+	)
+	if err := result.Decode(&updatedUser); err != nil {
+		return nil, errors.New("no document with that id exists")
+	}
+	return updatedUser, nil
 }
